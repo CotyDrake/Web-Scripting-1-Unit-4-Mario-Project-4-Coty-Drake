397646696ee661f3d9c2ab5e7b36fb71cac0f71b
coty drake <99136368+CotyDrake@users.noreply.github.com>
2022-11-01 22:03:18 +0000

    I added a text box and a button with css. I also created a function that takes the number inputted in text box an creates a pyramid with that many rows when you push the button.

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+var x = document.getElementById("myText").value;
+ 
+let string = "";
+ for (let i = 1; i <= x; i++) {
+  // printing spaces
+  for (let j = 0; j < x - i; j++) {
+    string +="";
+  }
+  // printing star
+  for (let k = 0; k < i; k++) {
+    string += "**";
+  }
+  string += "<br>";
+}
+document.getElementById("pyramid").innerHTML = string;
+}
